9f9a6874b5c913710d4e0d1f3abdaa9394625498
Dinesh Gupta <161644718+987Learner108@users.noreply.github.com>
2024-12-02 18:15:29 +0000

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const account_Id= 144553
+let account_Email= "987dineshgupta@gamail.com";
+var account_Password = '12345';
+account_City = "Jaipur";// we can also declare the variable without any declaration syntax
+
+// Prefer not to use 'var' beacause of issue in block scope ab=nd functional scope
+
+//account_Id= 122334
+console.log(account_Id);
+
+account_Email='hello@hc.com';
+account_Password = "34567";
+account_City="Butwal"
+
+console.table([account_Email, account_Id, account_Password, account_City])
