aeaaecdba5f9730775c7f8d72a39644313655b99
Dinesh Gupta <161644718+987Learner108@users.noreply.github.com>
2024-12-02 18:30:48 +0000

    second commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all javascript code as newer version
+
+//alert( 3 + 3) we are using nodejs, not browser
+
+//console.log(3 + 3);
+
+let name = "Dinesh";
+let age = 18;
+let isLoggedIn = false
+
+// number => 2 tompower 53
+//bigint
+//string = " "
+// boolean = true/false
+// null => standalone value
+// undefined => value is not assigned 
+// symbol => it is use for unique
+
+// object 
+
+console.log(typeof "Dinesh");
